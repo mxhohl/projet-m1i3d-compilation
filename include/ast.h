68d1e19393a3ea8cc32ba23b6f2b66d678f94809
Maxime Hohl <maxime.hohl@orange.fr>
2019-11-19 14:28:03 +0100

    AST nodes types added

--- include/ast.h
+++ include/ast.h
@@ -2,16 +2,56 @@
 #define HEADER_AST
 
 typedef enum e_NodeType {
-    CONST_FLOAT,
-    CONST_INT
+    STATIC_DOUBLE,
+    STATIC_INT,
+    PLUS,
+    MINUS,
+    ADD,
+    SUBSTRACT,
+    MULTIPLY,
+    DIVIDE,
+    INCREMENT,
+    DECREMENT,
+    BRANCH,
+    LOOP
 } NodeType;
+
+struct s_astNode;
+
+typedef struct s_astNode* astUnaryOperator;
+
+typedef struct s_astBinaryOperator {
+    struct s_astNode* right;
+    struct s_astNode* left;
+} astBinaryOperator;
+
+typedef struct s_astBranch {
+    struct s_astNode* condition;
+    struct s_astNode* ifBody;
+    struct s_astNode* elseBody;
+} astBranch;
+
+typedef struct s_astLoop {
+    struct s_astNode* condition;
+    struct s_astNode* body;
+} astLoop;
 
 typedef struct s_astNode {
     NodeType type;
 
     union {
-        float floatValue;
-        int intValue;
+        double staticDouble;
+        int staticInt;
+        astBinaryOperator opAdd;
+        astBinaryOperator opSubstract;
+        astBinaryOperator opMultiply;
+        astBinaryOperator opDivide;
+        astUnaryOperator opPlus;
+        astUnaryOperator opMinus;
+        astUnaryOperator opIncrement;
+        astUnaryOperator opDecrement;
+        astBranch branch;
+        astLoop loop;
     };
 
 } ASTNode;
